a6d70ba0534eb7eea196355667ed4445a0979096
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-05-03 06:42:37 -0700

    Q27 ts/js node js of 45 assignments
    Q27 ts/js node js of 45 assignments

---
+++ q27.js
@@ -0,0 +1,38 @@
+//Q27.Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+//• If the alien is green, print a message that the player earned 5 points.
+//• If the alien is yellow, print a message that the player earned 10 points.
+//• If the alien is red, print a message that the player earned 15 points.
+//• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+//for Green: :~If you want to print  only green color then comment remaining 2 colors
+var alienColor = "green";
+if (alienColor === "green") {
+    console.log("plyer just earned 5 point!");
+}
+else if (alienColor === "yellow") {
+    console.log("player just earned 10 point!");
+}
+else if (alienColor === "red") {
+    console.log("player just earned 15 points!");
+}
+//for Yellow: :~If you want to print  only yellow color then comment remaining 2 colors
+var alienColor = "Yellow";
+if (alienColor === "Yellow") {
+    console.log("player just earned only 5 points!");
+}
+else if (alienColor === "green") {
+    console.log("player just earned only 10 points");
+}
+else if (alienColor === "red") {
+    console.log("player just earned 15 points!");
+}
+//for Red :~If you want to print  only Red color then comment remaining 2 colors
+var alienColor = "red";
+if (alienColor === "red") {
+    console.log(" player just earned 5 points!");
+}
+else if (alienColor === "green") {
+    console.log("player just earned only 10 points!");
+}
+else if (alienColor === "yellow") {
+    console.log("player just earned only 15 points!");
+}
